1f807ff0eecb27a51566771731dceb2a44618e3a
somtom <som_tom@web.de>
2020-07-30 07:22:40 +0200

    made footer sticky

--- components/Footer/index.vue
+++ components/Footer/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="pt-5 bg-gray-800 w-full h-20 text-center text-white">
+  <div
+    class="absolute bottom-0 pt-5 bg-gray-800 w-full h-20 text-center text-white"
+  >
     <nuxt-link to="/impressum"> Impressum </nuxt-link>
   </div>
 </template>
